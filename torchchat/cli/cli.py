b17ac74fca3381331783928cff4c824d56c8e1aa
zengxian <xiangdong.zeng@intel.com>
2024-11-17 05:29:11 -0500

    enable benchmark

--- torchchat/cli/cli.py
+++ torchchat/cli/cli.py
@@ -405,6 +405,8 @@ def _add_generation_args(parser, verb: str) -> None:
         action="store_true",
         help="Whether to use max-autotune.",
     )
+    generator_parser.add_argument("--num-iter", default=10, type=int, help="num iter")
+    generator_parser.add_argument("--num-warmup", default=3, type=int, help="num warmup")
 
 
 # Add CLI Args specific to Model Evaluation
